c1a8548a37162f38dd82401627ab089f3d3136a8
JoshuaMEllis <joshuamellis5@gmail.com>
2019-11-22 18:17:39 +0000

    Updated horizontal movement
    Changed from Rigidbody2D.velocity to Rigidbody2D.AddForce

--- Orbit/Assets/Scripts/PlayerScript.cs
+++ Orbit/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -8,21 +6,15 @@ public class PlayerScript : MonoBehaviour
 
     private Rigidbody2D myBody;
 
-    public float baseMoveSpeed;
-    public float acceleration;
-    public bool accelerating;
-    public long startTime;
-
-    // Use this for initialization
+    public float horizontalThrust;
+    
     void Awake()
     {
-        baseMoveSpeed = 0.005F;
-        acceleration = 1.0F;
+        horizontalThrust = 1;
         Physics2D.gravity = Vector2.zero;
         myBody = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         Move();
@@ -30,38 +22,6 @@ public class PlayerScript : MonoBehaviour
 
     void Move()
     {
-        StartAccelerating();
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            myBody.velocity = new Vector2(baseMoveSpeed * acceleration, myBody.velocity.y);
-            Accelerate();
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            myBody.velocity = new Vector2(-(baseMoveSpeed * acceleration), myBody.velocity.y);
-            Accelerate();
-        }
-        else if (Input.GetAxisRaw("Horizontal") == 0)
-        {
-            myBody.velocity = new Vector2(0, myBody.velocity.y);
-            accelerating = false;
-        }
-    }
-
-    void Accelerate()
-    {
-        DateTime now = DateTime.Now;
-        int nowSeconds = now.Millisecond + (now.Second + (now.Minute * 60) + (now.Hour * 3600)) * 1000;
-        acceleration = (nowSeconds - startTime);
-    }
-
-    void StartAccelerating()
-    {
-        if (!accelerating)
-        {
-            DateTime now = DateTime.Now;
-            startTime = now.Millisecond + (now.Second + (now.Minute * 60) + (now.Hour * 3600)) * 1000;
-        }
-        accelerating = true;
+        myBody.AddForce(new Vector2(horizontalThrust * Input.GetAxisRaw("Horizontal"), 0));
     }
 }
